827ce748302dd86fa5fb7ba7fb396a76e1b295ac
Ethan Kovalenko <ashervoronin@gmail.com>
2021-02-07 16:12:15 -0600

    Redid HelloController to work with Razor, and added some Razor templates.

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -6,43 +6,20 @@ using System.Threading.Tasks;
 
 namespace HelloASPDotNET.Controllers
 {
-    [Route("/helloworld/")] // DRY version for methods
     public class HelloController : Controller // Extends Controller
     {
-        // GET: /<controller>/
-        // Add an HTTP verb attribute - that specifies the request type,
-        // we want a GET: request. Tell the index you only want to respond
-        // to the GET: request
         [HttpGet]
-        // [Route("/helloworld/")] // Gives the path information
         public IActionResult Index() // First method
         {
-            // Want it to return a form
-            string html = "<form method='post' action='/helloworld/'>" +
-                "<input type='text' name='name' />" +
-                "<input type='submit' value='Greet Me!' />" +
-                "</form>";
-
-            // This is an IActionResult return that allows us to return
-            // html, .json, etc.
-            // Need to specify variable it is returning as well as its type
-            return Content(html, "text/html");
+            return View();
         }
 
-        // Add arguments to the action method
-        // GET: /hello/welcome
-        // [HttpGet]
-        // [Route("/helloworld/welcome/{name?}")] // This says where we want to go
-        // The ? designates the name as optional
-        // helloworld/welcome/Bryan
-
-        [HttpGet("/welcome/{name?}")] // Short version of above for DRY
         [HttpPost]
-        // [Route("/helloworld/")]
+        [Route("/hello/")]
         public IActionResult Welcome(string name = "World") // Default value is World
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>",
-                "text/html");
+            ViewBag.person = name;
+            return View();
         }
     }
 }
